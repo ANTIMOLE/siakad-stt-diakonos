e2a513ca9f1fcd0a07b3c2ee7647120e17765387
ANTIMOLE <angellokhara4672@gmail.com>
2026-02-12 15:02:53 +0700

    testrender

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,20 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//    output: 'standalone',
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   output: 'standalone',
+  output: 'export',  // ← GANTI dari 'standalone' jadi 'export'
+  images: {
+    unoptimized: true  // ← TAMBAHKAN ini
+  }
+  // config lain tetap
 };
 
 export default nextConfig;
